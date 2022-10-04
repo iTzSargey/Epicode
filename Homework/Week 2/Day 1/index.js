7877e0d6a47fbd843a5402e1257313edb661a96b
iTzSargey <jordonsargerson@btinternet.com>
2022-10-04 12:36:51 +0100

    Commit

---
+++ Homework/Week 2/Day 1/index.js
@@ -0,0 +1,9 @@
+let firstname = "Jordon";
+let middlename = "Paul";
+let lastname = "Sargerson";
+let age = "29";
+let year = "2022";
+let space = " ";
+
+console.log(firstname + space + middlename + space + lastname);
+console.log(year - age);
